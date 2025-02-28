7bbdff90e708757d4fb6e1bfea6a047e04f1999a
Anand <91796691+anandvsinh@users.noreply.github.com>
2025-02-28 12:46:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://<db_username>:<db_password>@cluster0.0ued8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
@@ -15,8 +15,8 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "XX",
-      password: "enteryourcustompass",
+      name: "GTP Music",
+      password: "anand@123",
       host: "180.188.226.76",
       port:  7019,
       secure: false
