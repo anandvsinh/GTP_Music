ed72751471c51c139dda8bf8e47618143f43b194
Anand <91796691+anandvsinh@users.noreply.github.com>
2025-02-28 12:58:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 
 
 module.exports = {
-  TOKEN: "",
+  TOKEN: "MTM0MTUxNTUyODE0OTE0MzYzMg.G8eAMY.otjw69ecECecLH__-myplzGHqtiYY3auS8_Fec",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "mongodb+srv://<db_username>:<db_password>@cluster0.0ued8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  ownerID: ["861185736003026985", ""], 
+  mongodbUri : "mongodb+srv://anandvsinh:anand@123@cluster0.0ued8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
